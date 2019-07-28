1d83aa84482d24920a15210970111034eb7deded
Alex Mason <alexgmason@gmail.com>
2019-07-28 20:05:17 +0100

    Add grid constructor

---
+++ grid/index.js
@@ -0,0 +1,6 @@
+function Grid(width, height) {
+  this.width = width
+  this.height = height
+}
+
+module.exports = Grid
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const input = require('./input')
 const parse = require('./parse')
 
+const Grid = require('./Grid')
+
 function getInput() {
   input()
   .then(result => {
@@ -10,6 +12,11 @@ function getInput() {
 
     console.log('gridData', gridData)
     console.log('shipsData', shipsData)
+
+    // Create a grid of the correct size
+    const grid = new Grid(gridData.width, gridData.height)
+
+    console.log('grid', grid)
   })
 }
 
