945259905e6a57e57c011a0729af782181382058
Alex Mason <alexgmason@gmail.com>
2019-07-28 22:42:47 +0100

    Remove logs from index.js file. Also catch errors and print to console

--- index.js
+++ index.js
@@ -7,25 +7,21 @@ const Ship = require('./Ship')
 function getInput() {
   input()
   .then(result => {
-    console.log('result', result)
-
-    const {gridData, shipsData} = parse(result)
-
-    console.log('gridData', gridData)
-    console.log('shipsData', shipsData)
-
-    // Create a grid of the correct size
-    const grid = new Grid(gridData.width, gridData.height)
-
-    console.log('grid', grid)
-
-    // Loop through each ship and run it's instructions
-    shipsData.forEach((shipData, i) => {
-      const ship = new Ship(shipData.coordinates, shipData.instructions, grid)
-
-      ship.run()
-    })
-
+    try{
+      const {gridData, shipsData} = parse(result)
+  
+      // Create a grid of the correct size
+      const grid = new Grid(gridData.width, gridData.height)
+  
+      // Loop through each ship and run it's instructions
+      shipsData.forEach((shipData, i) => {
+        const ship = new Ship(shipData.coordinates, shipData.instructions, grid)
+  
+        ship.run()
+      })
+    } catch (error) {
+      console.log(error.message)
+    }
     
   })
 }
